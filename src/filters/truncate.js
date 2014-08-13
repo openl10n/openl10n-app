4fd521b7884d636b6f37eb837bb50f5bac2c3d27
Matthieu Moquet <matthieu@moquet.net>
2014-08-13 23:54:48 +0200

    Create first test

--- src/filters/truncate.js
+++ src/filters/truncate.js
@@ -1,5 +1,22 @@
-app.filter('truncate', function() {
-  return function(input, length, chars) {
+(function() {
+
+angular
+  .module('app')
+  .filter('truncate', truncateFilter);
+
+function truncateFilter() {
+
+  /**
+   * @name truncate
+   * @desc Truncate the input text
+   * @param {String} input  The text to truncate
+   * @param {String} length The max length of the string
+   * @param {String} chars  The ending char
+   * @return {String} The truncated string
+   */
+  return function truncate(input, length, chars) {
     return S(input).truncate(length, chars).s;
   };
-});
+}
+
+})();
