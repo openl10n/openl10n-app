b4b728487c7d69eed4522aec94f8d41a8a973ea9
Matthieu Moquet <matthieu@moquet.net>
2014-08-16 17:55:26 +0200

    Wrap ProjectController with IIFE

--- src/filters/truncate.js
+++ src/filters/truncate.js
@@ -1,5 +1,7 @@
 (function() {
 
+'use strict';
+
 angular
   .module('app')
   .filter('truncate', truncateFilter);
@@ -8,11 +10,16 @@ function truncateFilter() {
 
   /**
    * @name truncate
+   *
    * @desc Truncate the input text
+   *
    * @param {String} input  The text to truncate
    * @param {String} length The max length of the string
    * @param {String} chars  The ending char
+   *
    * @return {String} The truncated string
+   *
+   * @ngInject
    */
   return function truncate(input, length, chars) {
     return S(input).truncate(length, chars).s;
