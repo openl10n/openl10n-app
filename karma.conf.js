de3afc118ce55439589e76b05b9c850292114795
Matthieu Moquet <matthieu@moquet.net>
2014-08-16 18:18:06 +0200

    Remaned spec files pattern

--- karma.conf.js
+++ karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function(config) {
       'bower_components/angular-mocks/angular-mocks.js',
       'bower_components/angular-ui-router/release/angular-ui-router.js',
       'src/**/*.js',
-      'test/spec/**/*.js'
+      'test/**/*.js'
     ],
 
 
