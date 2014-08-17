7dd68560126717d7bbb624531c136171cf0a9690
Matthieu Moquet <matthieu@moquet.net>
2014-08-17 13:00:59 +0200

    Better ui feedback for login failure

--- src/controllers/LoginController.js
+++ src/controllers/LoginController.js
@@ -1,16 +1,24 @@
-app.controller('LoginController', function ($rootScope, $scope, $location, AuthenticationService, AuthEvents) {
+app.controller('LoginController', function ($rootScope, $scope, $location, $timeout, AuthenticationService, AuthEvents) {
   $scope.credentials = {
     username: '',
     password: '',
   };
 
+  $scope.error = false;
+
   $scope.authenticate = function (username, password) {
+    $scope.error = false;
+
     AuthenticationService.login(username, password).then(function() {
       $rootScope.$broadcast(AuthEvents.LOGIN_SUCCESS);
       $location.path('/');
     }, function() {
       $rootScope.$broadcast(AuthEvents.LOGIN_FAILED);
-      alert('Login failed');
+      $scope.error = true;
+
+      $timeout(function() {
+        $scope.error = false;
+      }, 1000);
     })
   };
 })
