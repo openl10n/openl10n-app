b4a4688f4e89ce699741d05c05c65e59c39ff3f1
Matthieu Moquet <matthieu@moquet.net>
2014-08-20 22:35:13 +0200

    Change sessionStorage to localStorage

--- src/factory/Session.js
+++ src/factory/Session.js
@@ -1,18 +1,18 @@
 app.service('Session', function($window) {
   this.create = function(token) {
-    $window.sessionStorage.token = token;
+    $window.localStorage.token = token;
   }
 
   this.destroy = function() {
-    delete $window.sessionStorage.token;
+    delete $window.localStorage.token;
   }
 
   this.getToken = function(token) {
-    return $window.sessionStorage.token;
+    return $window.localStorage.token;
   }
 
   this.isAuthenticated = function() {
-    return !!$window.sessionStorage.token;
+    return !!$window.localStorage.token;
   }
 
   return this;
