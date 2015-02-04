6640e01fbfb865090f58e826575935637b763041
Matthieu Moquet <matthieu@moquet.net>
2015-02-04 18:38:35 +0100

    Add new flag mapping

--- src/filters/region.js
+++ src/filters/region.js
@@ -7,10 +7,10 @@ angular
   .filter('region', regionFilter);
 
 var mapping = {
-  'ar': 'AE', 'be': 'BY', 'bg': 'BG', 'ca': 'ES', 'cs': 'CZ', 'da': 'DK', 'de': 'DE',
-  'el': 'GR', 'en': 'GB', 'es': 'ES', 'et': 'EE', 'fi': 'FI', 'fr': 'FR', 'ga': 'IE',
-  'hi': 'IN', 'hr': 'HR', 'hu': 'HU', 'id': 'ID', 'in': 'ID', 'is': 'IS', 'it': 'IT',
-  'iw': 'IL', 'ja': 'JP', 'ko': 'KR', 'lt': 'LT', 'lv': 'LV', 'mk': 'MK', 'ms': 'MY',
+  'af': 'ZA', 'ar': 'AE', 'az': 'AZ', 'be': 'BY', 'bg': 'BG', 'ca': 'ES', 'cs': 'CZ', 'da': 'DK', 'de': 'DE',
+  'el': 'GR', 'en': 'GB', 'es': 'ES', 'et': 'EE', 'fa': 'IR', 'fi': 'FI', 'fr': 'FR', 'ga': 'IE', 'he': 'IL',
+  'hi': 'IN', 'hr': 'HR', 'hu': 'HU', 'hy': 'AM', 'id': 'ID', 'in': 'ID', 'is': 'IS', 'it': 'IT',
+  'iw': 'IL', 'ja': 'JP', 'ko': 'KR', 'lt': 'LT', 'lv': 'LV', 'lb': 'LU', 'mk': 'MK', 'mn': 'MN', 'ms': 'MY',
   'mt': 'MT', 'nl': 'NL', 'no': 'NO', 'pl': 'PL', 'pt': 'PT', 'ro': 'RO', 'ru': 'RU',
   'sk': 'SK', 'sl': 'SI', 'sq': 'AL', 'sr': 'RS', 'sv': 'SE', 'th': 'TH', 'tr': 'TR',
   'uk': 'UA', 'vi': 'VN', 'zh': 'CN'
