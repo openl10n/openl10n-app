e2b99dff2e4dc48c43955104b5a4173d46c85725
Matthieu Moquet <matthieu@moquet.net>
2015-02-09 22:14:58 +0100

    Update app bootstrap

--- src/core/app.js
+++ src/core/app.js
@@ -6,7 +6,7 @@ angular.module('app', [
   'ngMessages',
   'ngMaterial',
   'ui.router',
-  // 'ui.gravatar',
+  'ui.gravatar',
   'cfp.hotkeys',
   'infinite-scroll',
   'monospaced.elastic',
@@ -16,20 +16,21 @@ angular.module('app', [
   // Application specific
   'templates',
 
-]).run(function($document) {
-  // Initialize the page body.
-  // This set the body attributes and inject the default html template.
+]).run(initialization);
+
+/**
+ * Initialize the page body.
+ *
+ * This function set the body attributes and inject the default html template.
+ *
+ * @ngInject
+ */
+function initialization($document) {
   var body = $document[0].body;
+
   body.setAttribute('layout', 'row');
-  body.innerHTML = [
-    '<div flex layout="column" ui-view></div>'
-    // '<div ng-include="\'views/sidebar.html\'"></div>',
-    // '<div layout="column" layout-fill>',
-    //   '<div ng-include="\'views/toolbar.html\'"></div>',
-    //   '<md-content md-scroll-y flex ui-view></md-content>',
-    // '</div>',
-  ].join('');
-});
+  body.innerHTML = '<div flex layout="column" ui-view></div>';
+}
 
 })();
 
--- src/core/bootstrap.js
+++ src/core/bootstrap.js
@@ -1,3 +1,4 @@
+document.body.setAttribute('ng-strict-di', '');
 document.body.setAttribute('ng-app', 'app');
 
 // $(function() {
