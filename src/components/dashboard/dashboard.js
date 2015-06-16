93407ff62fe83a774b32bac41911d5d7caa60454
Matthieu Moquet <matthieu@moquet.net>
2015-06-16 11:48:03 +0200

    Basic MD usage

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -1,10 +1,23 @@
-import {controller} from '../../config/decorators';
+import {controller, inject} from '../../config/decorators';
 
-/* jshint ignore:start */
 @controller
-/* jshint ignore:end */
 export class DashboardController {
+  @inject
+  $mdDialog = null;
+
   constructor() {
     this.text = 'world';
+  }
+
+  showAlert(event) {
+    this.$mdDialog.show(
+      this.$mdDialog.alert()
+        .parent(angular.element(document.body))
+        .title('This is an alert title')
+        .content('You can specify some description text in here.')
+        .ariaLabel('Alert Dialog Demo')
+        .ok('Got it!')
+        .targetEvent(event)
+    );
   }
 }
