9c782f25c768a788401fbdadfbaeec80380d0704
Matthieu Moquet <matthieu@moquet.net>
2015-06-17 18:28:34 +0200

    mdSelect miss animation

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -7,6 +7,11 @@ export class DashboardController {
 
   constructor() {
     this.text = 'world';
+
+    this.state = null;
+    this.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+      'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+      'WY').split(' ').map(function (state) { return { abbrev: state }; });
   }
 
   showAlert(event) {
